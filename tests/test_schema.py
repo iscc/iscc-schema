37e4ff53bcee7013c9da5abe2e9afc0818b40f33
Titusz Pan <titusz.pan@gmail.com>
2022-01-02 17:23:08 +0100

    Tweak metadata.

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -3,9 +3,9 @@ import iscc_schema
 
 
 def test_schema():
-    so = iscc_schema.IsccMetadataSchema(iscc="ISCC:EIAGUJFCEY")
+    so = iscc_schema.ISCC(iscc="ISCC:EIAGUJFCEY")
     assert so.dict(exclude_none=True, by_alias=True) == {
-        "@context": "https://purl.org/iscc/context/0.2.0.json",
-        "$schema": "https://purl.org/iscc/schema/0.2.0.json",
+        "@context": "https://purl.org/iscc/context/iscc.json",
+        "$schema": "https://purl.org/iscc/schema/iscc.json",
         "iscc": "ISCC:EIAGUJFCEY",
     }
