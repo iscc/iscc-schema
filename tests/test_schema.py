c72b591fd15b2b9a319220e9807b205cb385c3cf
Titusz Pan <titusz.pan@gmail.com>
2022-01-03 15:32:37 +0100

    Update tests.

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -5,7 +5,7 @@ import iscc_schema
 def test_schema():
     so = iscc_schema.ISCC(iscc="ISCC:EIAGUJFCEY")
     assert so.dict(exclude_none=True, by_alias=True) == {
-        "@context": "https://purl.org/iscc/context/iscc.json",
-        "$schema": "https://purl.org/iscc/schema/iscc.json",
+        "@context": "https://purl.org/iscc/terms/",
+        "$schema": "https://purl.org/iscc/schema/",
         "iscc": "ISCC:EIAGUJFCEY",
     }
