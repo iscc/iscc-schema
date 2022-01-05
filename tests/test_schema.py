3655daaf0375d832855ae7add8e12cb15dcfde22
Titusz Pan <titusz.pan@gmail.com>
2022-01-05 16:16:24 +0100

    Start adding more tests.

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import iscc_schema
+from pyld import jsonld
 
 
 def test_schema():
@@ -9,3 +10,19 @@ def test_schema():
         "$schema": "https://purl.org/iscc/schema/",
         "iscc": "ISCC:EIAGUJFCEY",
     }
+
+
+def test_json_ld_normalize():
+    so = iscc_schema.ISCC(iscc="ISCC:EIAGUJFCEY")
+    so.context_ = "http://schema.iscc.codes/context/isccmeta.jsonld"
+    data = so.dict(exclude_none=True, by_alias=True)
+    assert data == {
+        '@context': 'http://schema.iscc.codes/context/isccmeta.jsonld',
+        '$schema': 'https://purl.org/iscc/schema/',
+        'iscc': 'ISCC:EIAGUJFCEY'
+    }
+
+    # jsonld.set_document_loader(jsonld.requests_document_loader(timeout=10))
+
+    # norm = jsonld.normalize(data, {"algorithm": "URDNA2015", "format": "application/n-quads"})
+    # assert norm == ""
