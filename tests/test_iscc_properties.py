892946070dfc72ebfd36974798f43f00750c6e38
Titusz Pan <titusz.pan@gmail.com>
2022-01-26 21:24:52 +0100

    Update tests.

--- tests/test_iscc_properties.py
+++ tests/test_iscc_properties.py
@@ -3,20 +3,22 @@ import pytest
 import base64
 from pydantic import ValidationError
 
-from iscc_schema.schema import IsccProperties
+from iscc_schema.schema import IsccNft
 
 
 def test_iscc_properties_base64():
     b64 = base64.b64encode(b"Hello!")
-    ip = IsccProperties(properties=b64)
-    assert ip.dict() == {"properties": "SGVsbG8h"}
+    ip = IsccNft(properties=b64)
+    assert ip.dict(exclude_unset=True) == {"properties": "SGVsbG8h"}
 
 
 def test_iscc_properties_object():
-    obj = IsccProperties(properties={"any": "kind", "off": 64.43, "object": True})
-    assert obj.dict() == {"properties": {"any": "kind", "object": True, "off": 64.43}}
+    obj = IsccNft(properties={"any": "kind", "off": 64.43, "object": True})
+    assert obj.dict(exclude_unset=True) == {
+        "properties": {"any": "kind", "object": True, "off": 64.43}
+    }
 
 
 def test_iscc_properties_raises():
     with pytest.raises(ValidationError):
-        IsccProperties(properties="Hello!")
+        IsccNft(properties="Hello!")
