dace8578dfde7ea94a5be8824845ef5b7ceeefd7
Titusz Pan <titusz.pan@gmail.com>
2025-02-11 14:17:33 +0100

    refactor: Update test cases to support ISCC v1 and v2 schemas

--- tests/test_iscc_minimal.py
+++ tests/test_iscc_minimal.py
@@ -1,10 +1,19 @@
 # -*- coding: utf-8 -*-
-import pytest
-from iscc_schema.schema import IsccMinimal
+from iscc_schema.schema import IsccMinimal as IsccMinimalV1
+from iscc_schema.schema_v2 import IsccMinimal as IsccMinimalV2
 
 
-def test_iscc_minimal_longest():
-    im = IsccMinimal(
+def test_iscc_minimal_longest_v1():
+    im = IsccMinimalV1(
+        iscc="ISCC:KED572P4AOF5K6QXQA4T6OJD5UGX7UBPFW2TVQNTHBCKFRFCANCZARQ4K6NSFZQSH4GQ"
+    )
+    assert im.dict() == {
+        "iscc": "ISCC:KED572P4AOF5K6QXQA4T6OJD5UGX7UBPFW2TVQNTHBCKFRFCANCZARQ4K6NSFZQSH4GQ"
+    }
+
+
+def test_iscc_minimal_longest_v2():
+    im = IsccMinimalV2(
         iscc="ISCC:KAD7LOFDIKZG5M426IITP2XOZ2S6YR3C4YNQ25URPKITNUL2NXLHU3SKFW336BFNK6WQ"
     )
     assert im.dict() == {
